d357e66ad5c79221cc23c5b1927dd9ba73ef6119
Josh Wills <jwills@cloudera.com>
2013-04-02 22:57:45 -0700

    Initial HCatalog WIP

---
+++ hcatalog/src/main/java/com/cloudera/science/ml/hcatalog/HCatSpec.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2013, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package com.cloudera.science.ml.hcatalog;
+
+import java.util.List;
+
+import org.apache.hcatalog.common.HCatException;
+import org.apache.hcatalog.data.schema.HCatSchema;
+
+import com.cloudera.science.ml.core.records.DataType;
+import com.cloudera.science.ml.core.records.FieldSpec;
+import com.cloudera.science.ml.core.records.Spec;
+
+/**
+ *
+ */
+public class HCatSpec implements Spec {
+
+  private final HCatSchema schema;
+  
+  public HCatSpec(HCatSchema schema) {
+    this.schema = schema;
+  }
+  
+  @Override
+  public DataType getDataType() {
+    return DataType.RECORD;
+  }
+
+  @Override
+  public int size() {
+    return schema.size();
+  }
+
+  @Override
+  public List<String> getFieldNames() {
+    return schema.getFieldNames();
+  }
+
+  @Override
+  public FieldSpec getField(int index) {
+    return new HCatFieldSpec(schema.get(index), index);
+  }
+
+  @Override
+  public FieldSpec getField(String fieldName) {
+    try {
+      return new HCatFieldSpec(schema.get(fieldName), schema.getPosition(fieldName));
+    } catch (HCatException e) {
+      throw new IllegalArgumentException("Invalid field name: " + fieldName, e);
+    }
+  }
+
+}
---
+++ hcatalog/src/main/java/com/cloudera/science/ml/hcatalog/HCatalogRecord.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2013, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+package com.cloudera.science.ml.hcatalog;
+
+import org.apache.hcatalog.common.HCatException;
+import org.apache.hcatalog.data.DefaultHCatRecord;
+import org.apache.hcatalog.data.HCatRecord;
+import org.apache.hcatalog.data.schema.HCatSchema;
+
+import com.cloudera.science.ml.core.records.Record;
+import com.cloudera.science.ml.core.records.Spec;
+
+/**
+ *
+ */
+public class HCatalogRecord implements Record {
+
+  private final HCatRecord impl;
+  private final HCatSchema schema;
+  
+  public HCatalogRecord(HCatRecord impl, HCatSchema schema) {
+    this.impl = impl;
+    this.schema = schema;
+  }
+  
+  @Override
+  public Spec getSpec() {
+    return new HCatSpec(schema);
+  }
+
+  @Override
+  public Record copy(boolean deep) {
+    if (deep) {
+      throw new UnsupportedOperationException("HCatalog does not support deep copies yet");
+    }
+    HCatRecord copy = new DefaultHCatRecord();
+    try {
+      copy.copy(impl);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+    return new HCatalogRecord(copy, schema);
+  }
+
+  @Override
+  public Object get(int index) {
+    return impl.get(index);
+  }
+
+  @Override
+  public String getAsString(int index) {
+    return get(index).toString();
+  }
+
+  @Override
+  public double getAsDouble(int index) {
+    Object ret = get(index);
+    if (ret instanceof Number) {
+      return ((Number) ret).doubleValue();
+    } else {
+      try {
+        return Double.valueOf(ret.toString());
+      } catch (NumberFormatException e) {
+        return Double.NaN;
+      }
+    }
+  }
+
+  @Override
+  public Boolean getBoolean(int index) {
+    return getBoolean(schema.getFieldNames().get(index));
+  }
+
+  @Override
+  public Boolean getBoolean(String fieldName) {
+    try {
+      return impl.getBoolean(fieldName, schema);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Double getDouble(int index) {
+    return getDouble(schema.getFieldNames().get(index));
+  }
+
+  @Override
+  public Double getDouble(String fieldName) {
+    try {
+      return impl.getDouble(fieldName, schema);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Integer getInteger(int index) {
+    return getInteger(schema.getFieldNames().get(index));
+  }
+
+  @Override
+  public Integer getInteger(String fieldName) {
+    try {
+      return impl.getInteger(fieldName, schema);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Long getLong(int index) {
+    return getLong(schema.getFieldNames().get(index));
+  }
+
+  @Override
+  public Long getLong(String fieldName) {
+    try {
+      return impl.getLong(fieldName, schema);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String getString(int index) {
+    return getString(schema.getFieldNames().get(index));
+  }
+
+  @Override
+  public String getString(String fieldName) {
+    try {
+      return impl.getString(fieldName, schema);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Record set(int index, Object value) {
+    impl.set(index, value);
+    return this;
+  }
+
+  @Override
+  public Record set(String fieldName, Object value) {
+    try {
+      impl.set(fieldName, schema, value);
+    } catch (HCatException e) {
+      throw new RuntimeException(e);
+    }
+    return this;
+  }
+
+}
