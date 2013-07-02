717963e176984fced9633a203b945c80241c6cfa
Sandy Ryza <sandy@cloudera.com>
2013-07-02 13:18:11 -0700

    Add test that verifies that CrossfoldFn works correctly with Crossfold

--- parallel/src/test/java/com/cloudera/science/ml/parallel/crossfold/TestCrossfoldFn.java
+++ parallel/src/test/java/com/cloudera/science/ml/parallel/crossfold/TestCrossfoldFn.java
@@ -60,6 +60,55 @@ public class TestCrossfoldFn {
     }
   }
   
+  @Test
+  public void testCrossfoldFnWithCrossfold() {
+    final int numFolds = 3;
+    CrossfoldFn<String> crossfoldFn = new CrossfoldFn<String>(numFolds, seed);
+    String[] strings = {"jim", "corbett", "william", "shirer", "raymond", "chandler",
+        "jk", "rowling", "salman", "rushdie", "thomas", "pynchon", "david",
+        "foster", "wallace"};
+    PCollection<String> pstrings = MemPipeline.typedCollectionOf(Avros.strings(),
+        strings);
+    PTypeFamily ptf = pstrings.getTypeFamily();
+    PCollection<Pair<Integer, String>> pcrossfolded =
+        pstrings.parallelDo(crossfoldFn, ptf.pairs(ptf.ints(), ptf.strings()));
+    
+    Crossfold crossfold = new Crossfold(numFolds, seed);
+    PCollection<Pair<Integer, String>> pcrossfolds = crossfold.apply(pstrings);
+    
+    Collection<Pair<Integer, String>> crossfolded =
+        new CollectionPObject<Pair<Integer, String>>(pcrossfolded).getValue();
+    Collection<Pair<Integer, String>> crossfolds =
+        new CollectionPObject<Pair<Integer, String>>(pcrossfolds).getValue();
+ 
+    Assert.assertEquals((numFolds-1) * strings.length, crossfolded.size());
+    Assert.assertEquals(strings.length, crossfolds.size());
+    
+    Multimap<Integer, String> crossfoldedMap = pairCollectionToMultimap(crossfolded);
+    Multimap<Integer, String> crossfoldsMap = pairCollectionToMultimap(crossfolds);
+    Assert.assertEquals(crossfoldedMap.keySet().size(), crossfoldsMap.keySet().size());
+    Assert.assertEquals(numFolds, crossfoldsMap.keySet().size());
+    
+    for (Integer fold : crossfoldedMap.keys()) {
+      Collection<String> crossfoldedItems = crossfoldedMap.get(fold);
+      Collection<String> crossfoldsItems = crossfoldsMap.get(fold);
+      Assert.assertEquals(strings.length, crossfoldedItems.size() + crossfoldsItems.size());
+      HashSet<String> uniques = new HashSet<String>();
+      uniques.addAll(crossfoldsItems);
+      uniques.addAll(crossfoldedItems);
+      Assert.assertEquals(strings.length, uniques.size());
+    }
+  }
+  
+  private <K, V> Multimap<K, V> pairCollectionToMultimap(
+      Collection<Pair<K, V>> collection) {
+    Multimap<K, V> map = ArrayListMultimap.create();
+    for (Pair<K, V> pair : collection) {
+      map.put(pair.first(), pair.second());
+    }
+    return map;
+  }
+  
   private <K, V> Multimap<K, V> pairCollectionToInvertedMultimap(
       Collection<Pair<V, K>> collection) {
     Multimap<K, V> map = ArrayListMultimap.create();
